0c22fd08c58f9038343c25d6311226477df02c11
thanhch <thanhch@nal.vn>
2025-12-17 11:17:21 +0700

    Add cloudwatch rum

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,30 @@
 import SnakesGame from "./SnakesGame";
+import { AwsRum, AwsRumConfig } from 'aws-rum-web';
+
+try {
+    const config: AwsRumConfig = {
+        sessionSampleRate: 1 ,
+        identityPoolId: "ap-southeast-1:c621b075-37ab-4979-a0c8-5a12d2905509" ,
+        endpoint: "https://dataplane.rum.ap-southeast-1.amazonaws.com" ,
+        telemetries: ["performance","errors","http"] ,
+        allowCookies: true ,
+        enableXRay: false ,
+        signing: true // If you have a public resource policy and wish to send unsigned requests please set this to false
+    };
+
+    const APPLICATION_ID: string = '02f4700a-b0dd-4dbd-b836-3cc435279d96';
+    const APPLICATION_VERSION: string = '1.0.0';
+    const APPLICATION_REGION: string = 'ap-southeast-1';
+
+    const awsRum: AwsRum = new AwsRum(
+        APPLICATION_ID,
+        APPLICATION_VERSION,
+        APPLICATION_REGION,
+        config
+    );
+} catch (error) {
+    // Ignore errors thrown during CloudWatch RUM web client initialization
+}
 
 function App() {
   return <SnakesGame />;
